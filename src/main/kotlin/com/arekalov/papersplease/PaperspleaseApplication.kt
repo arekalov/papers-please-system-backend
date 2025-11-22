a025c032b9baf7f13be6c09740a5d5ae10fecff7
arekalov <artyom.rekalov@gmail.com>
2025-11-22 11:57:44 +0300

    initial commit

---
+++ src/main/kotlin/com/arekalov/papersplease/PaperspleaseApplication.kt
@@ -0,0 +1,11 @@
+package com.arekalov.papersplease
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaperspleaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<PaperspleaseApplication>(*args)
+}
---
+++ src/test/kotlin/com/arekalov/papersplease/PaperspleaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.arekalov.papersplease
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaperspleaseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
