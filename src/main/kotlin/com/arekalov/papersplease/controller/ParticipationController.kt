ae0e4f8344ecfdfe9e1917025ef8e5a127234eda
arekalov <artyom.rekalov@gmail.com>
2025-11-23 19:16:36 +0300

    [fix] update ParticipationsController endpoint

--- src/main/kotlin/com/arekalov/papersplease/controller/ParticipationController.kt
+++ src/main/kotlin/com/arekalov/papersplease/controller/ParticipationController.kt
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/participationsKate")
+@RequestMapping("/api/v1/participations")
 @PreAuthorize("hasAnyRole('INSPECTOR', 'BOSS', 'SECURITY', 'GOD')")
 class ParticipationController(
     private val participationService: ParticipationService,
